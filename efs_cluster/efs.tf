a8458b3a2032cd731557f5127a52a6f3ca3b1910
riteshsoni10 <riteshk.296@gmail.com>
2020-08-30 04:34:32 +0530

    Added efs_clsuter resources

---
+++ efs_cluster/efs.tf
@@ -0,0 +1,40 @@
+## Security Group for EFS Cluster
+resource "aws_security_group" "efs_security_group"{
+	name = "allow_nfs_traffic"
+	description = "Allow NFS Server Port Traffic from VPC"
+	vpc_id = var.vpc_id
+
+	ingress {
+		description = "NFS Port"
+		from_port = 2049
+		to_port = 2049
+		protocol = "tcp"
+		cidr_blocks = [data.aws_vpc.vpc_details.cidr_block]
+	}
+	egress {
+		from_port = 0
+		to_port = 0
+		protocol = "-1"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+}
+
+
+# EFS Cluster
+resource "aws_efs_file_system" "nfs_server" {
+  creation_token = "efs-cluster"
+  tags = {
+    Name = "EFS_Cluster"
+  }
+}
+
+
+## EFS Mount Target Subnets
+resource "aws_efs_mount_target" "efs_mount_details"{
+	count          = length( data.aws_subnet_ids.vpc_details.ids)
+	file_system_id = aws_efs_file_system.nfs_server.id
+	subnet_id       = tolist(data.aws_subnet_ids.vpc_details.ids)[count.index]
+	security_groups = [aws_security_group.efs_security_group.id]
+}
+
+
--- main.tf
+++ main.tf
@@ -3,3 +3,8 @@ provider "aws"{
     profile = var.profile_name
 }
 
+module "efs" {
+    source = "./efs_cluster"
+    data.aws_subnet_ids.vpc_details.ids
+    data.aws_vpc.vpc_details.cidr_block
+}
