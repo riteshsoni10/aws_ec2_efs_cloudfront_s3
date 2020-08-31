89d7fdbe9eb682a88c6412753d106e1ae79e4970
Ritesh Kumar <riteshk.296@gmail.com>
2020-08-31 05:53:50 +0530

    Added warnings while usage of variables in production

--- automation-scripts/s3_bucket/vars.tf
+++ automation-scripts/s3_bucket/vars.tf
@@ -1,4 +1,4 @@
-
+## S3 Bucket variables
 variable "bucket_name" {
         type = string
 	description = "S3 Bucket Name for Images"
@@ -9,6 +9,7 @@ variable "bucket_acl" {
 	description = "ACL for S3 Bucket"
 }
 
+## Note: This vlue is set to true. Kindly set to false in Production environment
 variable "force_destroy_bucket" {
         type = bool
         description = "Parmater indicates that the objects from the bucket to be deleted, so that bucket can be destroyed without error"
