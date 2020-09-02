334c33c7477ed75a92a947829c37737d308b2d7b
Ritesh Kumar <riteshk.296@gmail.com>
2020-09-02 05:37:19 +0530

    Added some output variables

--- automation-scripts/cloudfront/outputs.tf
+++ automation-scripts/cloudfront/outputs.tf
@@ -1,3 +1,11 @@
 output "cdn_dns_name" {
 	value = aws_cloudfront_distribution.image_distribution.domain_name
 }
+
+output "cf_id" {
+  value       = aws_cloudfront_distribution.image_distribution.id
+}
+
+output "cf_status" {
+  value       = aws_cloudfront_distribution.image_distribution.status
+}
