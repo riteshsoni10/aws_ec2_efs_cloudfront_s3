4df721cb8ae951af213d44bda8aecc397dddacc0
Ritesh Kumar <riteshk.296@gmail.com>
2020-09-02 05:30:34 +0530

    Added EFS module outputs

--- automation-scripts/outputs.tf
+++ automation-scripts/outputs.tf
@@ -10,3 +10,14 @@ output "ec2_instance_public_ip"{
         value = module.instance_launch.public_ip
 }
 
+output "efs_cluster_dns_name" {
+        value = module.efs.efs_cluster_dns_name
+}
+
+output "efs_cluster_id"{
+        value = module.efs.cluster_id
+}
+
+output "efs_security_group_id"{
+        value = module.efs.security_group_id
+}
