dc91193d60436bb5519634336b3ed20b14f10709
Ritesh Kumar <riteshk.296@gmail.com>
2020-09-02 05:38:42 +0530

    Added EFS cluster output variables

--- automation-scripts/efs_cluster/outputs.tf
+++ automation-scripts/efs_cluster/outputs.tf
@@ -1,3 +1,13 @@
 output "efs_cluster_dns_name" {
     value = aws_efs_file_system.nfs_server.dns_name
 }
+
+
+output "cluster_id"{
+    value = aws_efs_file_system.nfs_server.id
+}
+
+
+output "security_group_id"{
+	value = aws_security_group.efs_security_group.id
+}
